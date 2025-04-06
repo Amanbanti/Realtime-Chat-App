719f65c5bd43bd0cabbb00b7a0bef156a9a812a2
Amanbanti <amanbanti2011@gmail.com>
2025-04-06 16:35:44 +0300

    message controller /send and read messages

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js"
 
 import authRoutes from "./route/auth.route.js"
+import messageRoutes from "./route/message.route.js"
+
 
 
 dotenv.config()
@@ -15,6 +17,9 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+
+app.use("/api/message", messageRoutes)
+
 
 app.listen (5001,()=>{
     console.log("Server is running on PORT:"+ port)
