dc88c2d40634b16c726590a7d50dd5abe49c1f02
Amanbanti <amanbanti2011@gmail.com>
2025-04-06 15:27:06 +0300

    auth update user Profile

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js"
 
 import authRoutes from "./route/auth.route.js"
@@ -11,6 +12,7 @@ const app = express()
 const port = process.env.PORT
 
 app.use(express.json());
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 
