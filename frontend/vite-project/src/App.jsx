a09a62e05e49269d1877699473751dd272dda752
Amanbanti <amanbanti2011@gmail.com>
2025-04-07 14:42:28 +0300

    frontend update

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'	
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline'>
+      Hello aman
+    </div>
+  )
+}
+
+export default App
+
