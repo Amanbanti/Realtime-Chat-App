2017356a04ad5d7b835be604d27b037053f3b110
Amanbanti <amanbanti2011@gmail.com>
2025-06-15 22:13:01 +0300

    frontend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js"
 
@@ -15,6 +16,10 @@ const port = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
 
 app.use("/api/auth", authRoutes)
 
