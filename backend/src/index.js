afe63d845cbabf9c98aeff7488bda03056a1c672
Amanbanti <amanbanti2011@gmail.com>
2025-06-18 15:49:42 +0300

    server update

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,10 +21,12 @@ const __dirname = path.resolve()
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser())
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}))
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
 
 app.use("/api/auth", authRoutes)
 
